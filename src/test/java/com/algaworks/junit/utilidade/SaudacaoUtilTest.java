f481dc603149b95de6a7db02665e035e2c3f9ccb
lelis <lelis685@gmail.com>
2023-01-20 07:40:37 -0300

    first commit

---
+++ src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
@@ -0,0 +1,17 @@
+package com.algaworks.junit.utilidade;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SaudacaoUtilTest {
+
+
+    @Test
+    public void saudar(){
+        String saudar = SaudacaoUtil.saudar(9);
+
+
+    }
+
+}
