f2e04f09453390b42c114a6a4d1634a3f196b8ec
lelis <lelis685@gmail.com>
2023-01-21 17:07:06 -0300

    test: customizando mock com thenAnswer

--- src/test/java/com/algaworks/junit/blog/negocio/CadastroEditorTest.java
+++ src/test/java/com/algaworks/junit/blog/negocio/CadastroEditorTest.java
@@ -1,32 +1,45 @@
 package com.algaworks.junit.blog.negocio;
 
+import com.algaworks.junit.blog.armazenamento.ArmazenamentoEditor;
 import com.algaworks.junit.blog.exception.RegraNegocioException;
 import com.algaworks.junit.blog.modelo.Editor;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoSettings;
 
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
+@MockitoSettings
 class CadastroEditorTest {
 
+    @Mock
+    GerenciadorEnvioEmail gerenciadorEnvioEmail;
+
+    @Mock
+    ArmazenamentoEditor armazenamentoEditor;
+
+    @InjectMocks
     CadastroEditor cadastroEditor;
+
     Editor editor;
-    ArmazamentoEditorFixoEmMemoria armazenamentoEditor;
 
     @BeforeEach
     void beforeEach(){
-        armazenamentoEditor = new ArmazamentoEditorFixoEmMemoria();
-        cadastroEditor = new CadastroEditor(armazenamentoEditor,
-                new GerenciadorEnvioEmail() {
-                    @Override
-                    void enviarEmail(Mensagem mensagem) {
-                        System.out.println("Enviando mensagem para: " + mensagem.getDestinatario());
-                    }
-                });
         editor = new Editor(null, "marco", "marco@mail.com", BigDecimal.TEN, true);
+
+        Mockito.when(armazenamentoEditor.salvar(editor))
+                .thenAnswer(invocationOnMock -> {
+                    Editor editorPassado = invocationOnMock.getArgument(0, Editor.class);
+                    editorPassado.setId(1L);
+                    return editorPassado;
+                });
     }
 
 
@@ -34,29 +47,28 @@ class CadastroEditorTest {
     public void dado_editor_valido_quando_criar_entao_retorna_id_cadastro(){
         Editor editorsalvo = cadastroEditor.criar(editor);
         assertEquals(1L, editorsalvo.getId());
-        assertTrue(armazenamentoEditor.chamouSalvar);
     }
 
-    @Test
-    public void Dado_um_editor_null_Quando_criar_Entao_deve_lancar_exception() {
-        assertThrows(NullPointerException.class, ()-> cadastroEditor.criar(null));
-        assertFalse(armazenamentoEditor.chamouSalvar);
-    }
-
-    @Test
-    void Dado_um_editor_com_email_existente_Quando_criar_Entao_deve_lancar_exception() {
-        editor.setEmail("alex.existe@email.com");
-        assertThrows(RegraNegocioException.class, ()-> cadastroEditor.criar(editor));
-    }
-
-    @Test
-    void Dado_um_editor_com_email_existente_Quando_criar_Entao_nao_deve_salvar() {
-        editor.setEmail("alex.existe@email.com");
-        try {
-            cadastroEditor.criar(editor);
-        } catch (RegraNegocioException e) { }
-        assertFalse(armazenamentoEditor.chamouSalvar);
-    }
+//    @Test
+//    public void Dado_um_editor_null_Quando_criar_Entao_deve_lancar_exception() {
+//        assertThrows(NullPointerException.class, ()-> cadastroEditor.criar(null));
+//        assertFalse(armazenamentoEditor.chamouSalvar);
+//    }
+//
+//    @Test
+//    void Dado_um_editor_com_email_existente_Quando_criar_Entao_deve_lancar_exception() {
+//        editor.setEmail("alex.existe@email.com");
+//        assertThrows(RegraNegocioException.class, ()-> cadastroEditor.criar(editor));
+//    }
+//
+//    @Test
+//    void Dado_um_editor_com_email_existente_Quando_criar_Entao_nao_deve_salvar() {
+//        editor.setEmail("alex.existe@email.com");
+//        try {
+//            cadastroEditor.criar(editor);
+//        } catch (RegraNegocioException e) { }
+//        assertFalse(armazenamentoEditor.chamouSalvar);
+//    }
 
 
 
